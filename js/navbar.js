a432682ea9e0a581f6c5cc19398eeb5dcd3db873
HyperHamster535 <hyperhamster534@outlook.com>
2019-12-01 10:00:55 +0000

    added link colouring depending on scroll height

--- js/navbar.js
+++ js/navbar.js
@@ -8,3 +8,28 @@ function toggleNav() {
         document.getElementById("menu-button").innerHTML = "menu_open";
     }
 }
+
+function setnavlinks() {
+    
+    projectsheight = 619;
+    aboutheight = 1949;
+    
+    scroll = window.scrollY
+
+    console.log(scroll)
+
+    if (scroll < projectsheight) {
+        document.getElementById("nav-home").style.color = "#68FE9A";
+        document.getElementById("nav-projects").style.color = "#FFFFFF";
+        document.getElementById("nav-about").style.color = "#FFFFFF";
+    } else if (scroll < aboutheight) {
+        document.getElementById("nav-home").style.color = "#FFFFFF";
+        document.getElementById("nav-projects").style.color = "#68FE9A";
+        document.getElementById("nav-about").style.color = "#FFFFFF";
+    } else {
+        document.getElementById("nav-home").style.color = "#FFFFFF";
+        document.getElementById("nav-projects").style.color = "#FFFFFF";
+        document.getElementById("nav-about").style.color = "#68FE9A";
+    }
+
+}
