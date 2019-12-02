551cbd33b6e4ceaccf824a6476e818991c9ea899
HyperHamster535 <hyperhamster534@outlook.com>
2019-12-02 17:51:11 +0000

    added youtube section

--- js/navbar.js
+++ js/navbar.js
@@ -1,5 +1,5 @@
-projectsheight = 730;
-aboutheight = 2066;
+projectsheight = 1426;
+aboutheight = 2751;
 
 function toggleNav() {
     var nav = document.getElementById("navbar");
@@ -14,6 +14,7 @@ function toggleNav() {
 
 function setnavlinks() {
     scroll = window.scrollY
+    // console.log(scroll)
 
     if (scroll < projectsheight) {
         document.getElementById("nav-home").style.color = "#68FE9A";
